25ad5d5ee92e2f68b4dbee1b8928cac167b64d15
Lindon <lindonng@hotmail.com>
2022-05-07 00:09:08 +0800

    update setup.bat

--- setup.bat
+++ setup.bat
@@ -13,31 +13,31 @@ where python >nul 2>&1 && (
   ) else (
     exit /b
   )
-  echo %var%|findstr /R "[3].[89]" >nul
+  echo %var%|findstr /R "[3].[8910]" >nul
   if errorlevel 1 (
       if "%var%"=="" goto redo
-      echo [ERROR] MobSF dependencies require Python 3.8/3.9. Your python points to %var%
+      echo [ERROR] MobSF dependencies require Python 3.8/3.9/3.10 Your python points to %var%
       exit /b
   ) else (
       echo [INSTALL] Found %var%
   )
 
-  rem Pip Check and Upgrade
-  pip >nul 2>&1 && (
-    echo [INSTALL] Found pip
-    python -m pip install --no-cache-dir --upgrade pip
-  ) || (
-    echo [ERROR] pip is not available in PATH
-    pause
-    exit /b
-  )
+  @REM rem Pip Check and Upgrade
+  @REM pip >nul 2>&1 && (
+  @REM   echo [INSTALL] Found pip
+  @REM   python -m pip install --no-cache-dir --upgrade pip
+  @REM ) || (
+  @REM   echo [ERROR] pip is not available in PATH
+  @REM   pause
+  @REM   exit /b
+  @REM )
 
   rem OpenSSL Check
   if exist "C:\\Program Files\\OpenSSL-Win64\\bin\\openssl.exe" (
     echo [INSTALL] Found OpenSSL executable
   ) else (
    echo [ERROR] OpenSSL executable not found in [C:\\Program Files\\OpenSSL-Win64\\bin\\openssl.exe]
-   echo [INFO] Install OpenSSL non-light version - https://slproweb.com/download/Win64OpenSSL-3_0_0.exe
+   echo [INFO] Install OpenSSL non-light version - https://slproweb.com/download/Win64OpenSSL-3_0_2.exe
    pause
    exit /b
   )
@@ -57,13 +57,15 @@ where python >nul 2>&1 && (
   rmdir "venv" /q /s >nul 2>&1
   python -m venv ./venv
   set venv=.\venv\Scripts\python
-  %venv% -m pip install --upgrade pip
+  @REM %venv% -m pip install --upgrade pip
 
   set LIB=C:\Program Files\OpenSSL-Win64\lib;%LIB%
   set INCLUDE=C:\Program Files\OpenSSL-Win64\include;%INCLUDE%
 
   echo [INSTALL] Installing Requirements
   %venv% -m pip install --no-cache-dir wheel
+  %venv% -m pip wheel --wheel-dir=yara-python-dex git+https://github.com/MobSF/yara-python-dex.git
+  %venv% -m pip install --no-index --find-links=yara-python-dex yara-python-dex
   %venv% -m pip install --no-cache-dir --use-deprecated=legacy-resolver -r requirements.txt
   
   echo [INSTALL] Clean Up
